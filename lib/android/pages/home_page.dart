86c85562fea78a08a0399f492c55006c596f995e
Abeltomas16 <abeljosetoms115@gmail.com>
2022-03-31 01:27:07 +0100

    FEAT: inserido o body da pagina

--- lib/android/pages/home_page.dart
+++ lib/android/pages/home_page.dart
@@ -14,34 +14,85 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                pinned: true,
-                floating: false,
-                expandedHeight: MediaQuery.of(context).size.height * 0.3,
-                backgroundColor: Colors.white,
-                elevation: 0.0,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  titlePadding: const EdgeInsets.only(top: 50.0),
-                  title: const Text(
-                    "Raiz Q",
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
+      backgroundColor: Colors.white,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              pinned: true,
+              floating: false,
+              expandedHeight: MediaQuery.of(context).size.height * 0.3,
+              backgroundColor: Colors.white,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                titlePadding: const EdgeInsets.only(top: 50.0),
+                title: const Text(
+                  "Raiz Q",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                background: Lottie.asset(
+                  "assets/img/calculate.json",
+                  animate: true,
+                ),
+              ),
+            )
+          ];
+        },
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10.0,
+              vertical: 100.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextFormField(
+                  controller: bloc.controller,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: "Insira o valor",
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  bloc.resultado,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      bloc.calculate();
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(24),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text("Achar raiz"),
                     ),
                   ),
-                  background:
-                      Lottie.asset("assets/img/calculate.json", animate: true),
-                ),
-              )
-            ];
-          },
-          body: Container(),
-        ));
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
