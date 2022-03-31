d09de5690002c9598f6715b9ac09a75942578310
Abeltomas16 <abeljosetoms115@gmail.com>
2022-03-31 01:28:10 +0100

    FEAT:  verificando se a plataforma é android

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'android/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  if (Platform.isAndroid) runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Raiz q',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
