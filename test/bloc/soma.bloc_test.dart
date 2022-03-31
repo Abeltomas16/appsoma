52cc4107305a2f76bee425260c57d8ca7ba3f1d5
Abeltomas16 <abeljosetoms115@gmail.com>
2022-03-31 01:19:04 +0100

    FEAT: validando o teste

--- test/bloc/soma.bloc_test.dart
+++ test/bloc/soma.bloc_test.dart
@@ -1,9 +1,18 @@
-import 'dart:math';
+import 'package:appsoma/bloc/soma.bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'dart:math' show sqrt;
 
 void main() {
+  final bloc = SomaBloc();
   test(
     'should return raiz quadrada',
-    () {},
+    () {
+      TextEditingController controller = bloc.controller;
+      controller.text = "5";
+      final esperado = 'Raiz quadrada de ${controller.text} é ${sqrt(5)}';
+      bloc.calculate();
+      expect(esperado, bloc.resultado);
+    },
   );
 }
