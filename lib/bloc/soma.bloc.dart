9f110a7020597ad65eca561231f3cb221af1d4cc
Abeltomas16 <abeljosetoms115@gmail.com>
2022-03-31 01:16:21 +0100

    FEAT:Criado objectos da classe

--- lib/bloc/soma.bloc.dart
+++ lib/bloc/soma.bloc.dart
@@ -1,1 +1,12 @@
-class SomaBloc {}
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class SomaBloc {
+  var controller = TextEditingController();
+  var resultado = 'prencha os valores';
+  calculate() {
+    double valor = double.parse(controller.text);
+    resultado =
+        'Raiz quadrada de ${controller.text} é ${sqrt(valor).toStringAsFixed(4)}';
+  }
+}
