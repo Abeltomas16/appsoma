f8e4bf53011119fa348258ea6f0a3dafe9514955
Abeltomas16 <abeljosetoms115@gmail.com>
2022-03-31 01:25:28 +0100

    FEAT: criado header da pagina

--- lib/android/pages/home_page.dart
+++ lib/android/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:appsoma/bloc/soma.bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,10 +13,35 @@ class _HomePageState extends State<HomePage> {
   var bloc = SomaBloc();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Olá"),
-      ),
-    );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                pinned: true,
+                floating: false,
+                expandedHeight: MediaQuery.of(context).size.height * 0.3,
+                backgroundColor: Colors.white,
+                elevation: 0.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  titlePadding: const EdgeInsets.only(top: 50.0),
+                  title: const Text(
+                    "Raiz Q",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                  background:
+                      Lottie.asset("assets/img/calculate.json", animate: true),
+                ),
+              )
+            ];
+          },
+          body: Container(),
+        ));
   }
 }
